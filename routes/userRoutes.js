6c24a0ef50e35ddcc53e91be5706d038b90b0b85
Reema0206 <moorthyreema@gmail.com>
2025-05-23 10:35:14 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import { regiter } from "../controler/userConmtroller.js";
+
+const userRoute = express.Router();
+
+// GET
+
+
+// POST
+
+userRoute.post("/register",regiter)
+
+
+// PUT
+
+
+// DELETE
+
+
+
+export default userRoute
